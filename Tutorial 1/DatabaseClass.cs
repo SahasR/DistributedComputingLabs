519a074677aac7391a102af0412ea00ec7dea0d5
SahasR <sahasrenuja@gmail.com>
2022-08-08 19:08:34 +0530

    Add project files.

---
+++ Tutorial 1/DatabaseClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_1
+{
+    public class DatabaseClass
+    {
+        List<DataStruct> dataStruct;
+
+        public DatabaseClass()
+        {
+            dataStruct = new List<DataStruct>();
+            uint pin;
+            uint acctNo;
+            string firstName;
+            string lastName;
+            int balance;
+            DatabaseGenerator newGenerator = new DatabaseGenerator();
+            for (int i = 0; i < 1000; i++)
+            {
+                newGenerator.GetNextAccount(out pin,out acctNo,out firstName,out lastName,out balance);
+                DataStruct currAccount = new DataStruct(acctNo, pin, balance, firstName, lastName);
+                dataStruct.Add(currAccount);
+            }
+        }
+    }
+}
---
+++ Tutorial 1/DatabaseGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_1
+{
+    internal class DatabaseGenerator
+    {
+        private string GetFirstName()
+        {
+            Random random = new Random();
+            string[] firstNames = { "Sahas", "Abinaya", "Venumi", "Kapila", "Anurudda", "Pathum" };
+            int randomNumber = random.Next(0, 6);
+            string firstName = firstNames[randomNumber];
+            return firstName;
+        }
+
+        private string GetLastName()
+        {
+            Random random = new Random();
+            string[] lastNames = { "Gunasekara", "Sritharan", "Kaluarachchi", "Kaluarachchi", "Padeniya", "Nissanka" };
+            int randomNumber = random.Next(0, 6);
+            string lastName = lastNames[randomNumber];
+            return lastName;
+        }
+
+        private uint GetPIN()
+        {
+            Random random = new Random();
+            uint pin = (uint)random.Next(0, 10000);
+            return pin;
+        }
+
+        private uint GetAcctNo()
+        {
+            Random random = new Random();
+            uint accNum = (uint)random.Next(0, 10000000);
+            return accNum;
+        }
+
+        private int GetBalance()
+        {
+            Random random = new Random();
+            int balance = random.Next(0, 1000000000);
+            return balance;
+        }
+
+        public void GetNextAccount(out uint pin, out uint acctNo, out string firstName, out string lastName, out int balance)
+        {
+            pin = GetPIN();
+            acctNo = GetAcctNo();
+            firstName = GetFirstName();
+            lastName = GetLastName();
+            balance = GetBalance();
+        }
+    }
+}
