e8877a3bdf18143efedd04bf11c26ce3ee80bbe0
SahasR <sahasrenuja@gmail.com>
2022-08-09 00:18:03 +0530

    Client Side Application Developed as well!

---
+++ BankClient/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BankServer;
+
+namespace BankClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BankServerInterface bank;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //This is a factory that generates remote connections to our remote class. This is what hides the RPC stuff!
+            ChannelFactory<BankServerInterface> bankFactory;
+            NetTcpBinding tcp = new NetTcpBinding();
+
+            //Set the URL and create the connection!
+            string URL = "net.tcp://localhost:8100/BankService";
+            EndpointAddress address = new EndpointAddress(URL);
+            bankFactory = new ChannelFactory<BankServerInterface>(tcp, address);
+            bank = bankFactory.CreateChannel();
+            //Also, tell me how many entries are there in the DB
+            TotalRecs.Text = bank.GetNumEntries().ToString();
+
+        }
+
+        private void Go_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 0;
+            string fName = "", lName = "";
+            int bal = 0;
+            uint acct = 0, pin = 0;
+
+            //On click, Get the index...
+            index = Int32.Parse(Index.Text);
+            bank.GetValuesForEntry(index, out acct, out pin, out bal, out fName, out lName);
+
+            FNameBox.Text = fName;
+            LNameBox.Text = lName;
+            Balance.Text = bal.ToString();
+            AcctNo.Text = acct.ToString();
+            Pin.Text = pin.ToString();
+        }
+    }
+}
