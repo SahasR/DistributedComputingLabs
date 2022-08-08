be8e1b912b591d9864dd86d3bde0c3f36939fb35
SahasR <sahasrenuja@gmail.com>
2022-08-08 23:11:05 +0530

    Server Side is Complete!, Everything seemingly works!

---
+++ BankServer/BankServerImplementation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseLib;
+
+namespace BankServer
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false)]
+    internal class BankServerImplementation : BankServerInterface
+    {
+        private DatabaseClass database;
+        public BankServerImplementation()
+        {
+            database = new DatabaseClass();
+        }
+        public int GetNumEntries()
+        {
+            return database.GetNumRecords();
+        }
+
+        public void GetValuesForEntry(int index, out uint acctNo, out uint pin, out int bal, out string fName, out string lName)
+        {
+            acctNo = database.GetAcctNoByIndex(index);
+            pin = database.GetPINByIndex(index);
+            bal = database.GetBalanceByIndex(index);
+            fName = database.GetFirstNameByIndex(index);
+            lName = database.GetLastNameByIndex(index);
+        }
+
+    }
+}
---
+++ BankServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Bank Database Server!");
+            //This is the actual host service System
+            ServiceHost host;
+            //This represents a tcp/ip binding in the Windows Network Stack
+            NetTcpBinding tcp = new NetTcpBinding();
+            //Bind Server to the implementation of BankServer
+            host = new ServiceHost(typeof(BankServerImplementation));
+            //Present the publicly accessible interface to the client. 0.0.0.0 tells .net to accept on any interface.
+            //:8100 means this will use port 8100. DataService is a name for the actual service, this can be any string.
+            host.AddServiceEndpoint(typeof(BankServerInterface), tcp, "net.tcp://0.0.0.0:8100/BankService");
+            //And open the host for business!
+            host.Open();
+            Console.WriteLine("System is online!");
+            Console.ReadLine();
+            //Don't forget to close the host after you're done!
+            host.Close();
+        }
+    }
+}
